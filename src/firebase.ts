78c12b680c44afae29eb972bfb589e0d87c38aa5
Amanda Kilkenny <amandakilkenny@rocketmail.com>
2026-01-04 06:47:35 +0000

    Move Firebase API key to env and fix firebase init

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXPhoIEo73-Q4eUAQ0Rx3VbrSMtLEd3w4",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "inkbound-society.firebaseapp.com",
   projectId: "inkbound-society",
   storageBucket: "inkbound-society.appspot.com",
   messagingSenderId: "850330029375",
-  appId: "1:850330029375:web:27c1146d41c42a9fa4578a"
+  appId: "1:850330029375:web:27c1146d41c42a9fa4578a",
 };
 
 const app = initializeApp(firebaseConfig);
